20b666736438fa550cb47987f9a29eb187597a83
Rudra <rudra17122005@gmail.com>
2025-11-06 04:59:01 +0530

    Add favicon assets and configure web manifest for Dental Solutions branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Favicon */}
+        <link rel="icon" type="image/x-icon" href="/assets/icons/favicon_io/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon_io/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon_io/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/favicon_io/apple-touch-icon.png" />
+        <link rel="manifest" href="/assets/icons/favicon_io/site.webmanifest" />
+        
         {/* Font Preloading for Performance */}
         <link
           rel="preload"
